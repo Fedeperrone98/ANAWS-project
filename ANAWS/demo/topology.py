f7fa1d2dfff80e5fa967a1685296722e800a9eba
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-02-24 18:34:14 +0100

    finish topology script

--- ANAWS/demo/topology.py
+++ ANAWS/demo/topology.py
@@ -1,42 +1,57 @@
-#!/usr/bin/env python
+"""
+Host: H_x_x_x
+Edge switch: es_x_x
+Aggregate switch: as_x_x
+Core switch: cs_x
+"""
 
-"""
-"""
 from mininet.topo import Topo
 
-class Tower( Topo ):
-    "Internet Topology Zoo Specimen."
+class FatTree(Topo):
 
-    def addSwitch( self, name, **opts ):
-        kwargs = { 'protocols' : 'OpenFlow13' }
-        kwargs.update( opts )
-        return super(Tower, self).addSwitch( name, **kwargs )
+    def __init__(self):
 
-    def __init__( self ):
-        "Create a topology."
+        # Topology settings
+        K = 4                           # K-ary FatTree
+        podNum = K                      # Pod number in FatTree
+        coreSwitchNum = pow((K/2),2)    # Core switches
+        aggrSwitchNum = ((K/2)*K)       # Aggregation switches
+        edgeSwitchNum = ((K/2)*K)       # Edge switches
+        hostNum = (K*pow((K/2),2))      # Hosts in K-ary FatTree
 
-        # Initialize Topology
-        Topo.__init__( self )
+        # Initialize topology
+        Topo.__init__(self)
 
-        spines = []
+        coreSwitches = []
+        aggrSwitches = []
+        edgeSwitches = []
 
-        # Create the two spine switches
-        spines.append(self.addSwitch( 's1' ))
-        spines.append(self.addSwitch( 's2' ))
+        # Core
+        for core in range(0, coreSwitchNum):
+            coreThis = self.addSwitch('cs_{}'.format(core), dpid='{:010X}'.format(core+1))
+            coreSwitches.append(coreThis)
 
-        # Now create the leaf switches, their hosts and connect them together
-        for i in range(4):
-            sn = i + 1
-            leaf = self.addSwitch( 's1%d' % sn )
-            for spine in spines:
-                self.addLink(leaf, spine)
+        # Pod
+        for pod in range(0, podNum):
+        # Aggregate
+            for aggr in range(0, aggrSwitchNum/podNum):
+                aggrThis = self.addSwitch('as_{}_{}'.format(pod, aggr), dpid='{:06X}{:02X}{:02X}'.format(1, pod, aggr))
+                aggrSwitches.append(aggrThis)
+                for x in range((K/2)*aggr, (K/2)*(aggr+1)):
+                    self.addLink(aggrThis, coreSwitches[x])
 
-            for j in range(5):
-                host = self.addHost( 'h%d%d' % (sn, j + 1) )
-                self.addLink( host, leaf )
+        # Edge
+            for edge in range(0, edgeSwitchNum/podNum):
+                edgeThis = self.addSwitch('es_{}_{}'.format(pod, edge), dpid='{:06X}{:02X}{:02X}'.format(2, pod, edge))
+                edgeSwitches.append(edgeThis)
+                for x in range((edgeSwitchNum/podNum)*pod, ((edgeSwitchNum/podNum)*(pod+1))):
+                    self.addLink(edgeThis, aggrSwitches[x])
 
-topos = { 'tower': ( lambda: Tower() ) }
+        # Host
+                for x in range(0, (hostNum/podNum/(edgeSwitchNum/podNum))):
+                    mac = '02:00:00:{:02}:{:02}:{:02}'.format(pod, edge, x)
+                    host = self.addHost('h_{}_{}_{}'.format(pod, edge, x), mac=mac, ip='10.{}.{}.{}'.format(pod, edge, x+1))
+                    self.addLink(edgeThis, host)
 
-if __name__ == '__main__':
-    from onosnet import run
-    run( Tower() )
+
+topos = { 'fattree': ( lambda: FatTree() ) }
