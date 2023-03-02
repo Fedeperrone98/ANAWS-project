ddb7e229bf6397b7857796dad97a4d2ae6cae97f
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-03-02 17:06:37 +0100

    Update FlowReservation.java

--- ANAWS/net/floodlightcontroller/unipi/flowreservation/FlowReservation.java
+++ ANAWS/net/floodlightcontroller/unipi/flowreservation/FlowReservation.java
@@ -1,25 +1,35 @@
 package net.floodlightcontroller.unipi.flowreservation;
 
+import java.util.ArrayList;
 import java.util.Collection;
+import java.util.HashMap;
 import java.util.Map;
 
 import org.projectfloodlight.openflow.protocol.OFMessage;
 import org.projectfloodlight.openflow.protocol.OFType;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import net.floodlightcontroller.core.FloodlightContext;
+import net.floodlightcontroller.core.IFloodlightProviderService;
 import net.floodlightcontroller.core.IOFMessageListener;
 import net.floodlightcontroller.core.IOFSwitch;
 import net.floodlightcontroller.core.module.FloodlightModuleContext;
 import net.floodlightcontroller.core.module.FloodlightModuleException;
 import net.floodlightcontroller.core.module.IFloodlightModule;
 import net.floodlightcontroller.core.module.IFloodlightService;
+import net.floodlightcontroller.restserver.IRestApiService;
 
 public class FlowReservation implements IFloodlightModule, IOFMessageListener, IFlowReservationREST {
 
+	protected IFloodlightProviderService floodlightProvider; // Reference to the provider
+	protected static Logger log;
+	
+	protected IRestApiService restApiService; // Reference to the Rest API service
+
 	@Override
 	public String getName() {
-		// TODO Auto-generated method stub
-		return null;
+		return FlowReservation.class.getSimpleName();
 	}
 
 	@Override
@@ -42,31 +52,50 @@ public class FlowReservation implements IFloodlightModule, IOFMessageListener, I
 
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleServices() {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<Class<? extends IFloodlightService>> l =
+				new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFlowReservationREST.class);
+		return l;
 	}
 
 	@Override
 	public Map<Class<? extends IFloodlightService>, IFloodlightService> getServiceImpls() {
-		// TODO Auto-generated method stub
-		return null;
+		Map<Class<? extends IFloodlightService>, IFloodlightService> m =
+				new HashMap<Class<? extends IFloodlightService>, IFloodlightService>();
+		m.put(IFlowReservationREST.class, this);
+		return m;
 	}
 
 	@Override
 	public Collection<Class<? extends IFloodlightService>> getModuleDependencies() {
-		// TODO Auto-generated method stub
-		return null;
+		Collection<Class<? extends IFloodlightService>> l =
+				new ArrayList<Class<? extends IFloodlightService>>();
+		l.add(IFloodlightProviderService.class);
+		
+		// Add among the dependences the RestApi service
+		l.add(IRestApiService.class);
+
+		return l;
 	}
 
 	@Override
 	public void init(FloodlightModuleContext context) throws FloodlightModuleException {
-		// TODO Auto-generated method stub
+		//inizializzo le variabili
+		floodlightProvider = context.getServiceImpl(IFloodlightProviderService.class);
+		log = LoggerFactory.getLogger(FlowReservation.class);
+		
+		// Retrieve a pointer to the rest api service
+	    restApiService = context.getServiceImpl(IRestApiService.class);
 
 	}
 
 	@Override
 	public void startUp(FloodlightModuleContext context) throws FloodlightModuleException {
-		// TODO Auto-generated method stub
+		//mi metto in attesa di packet in
+		floodlightProvider.addOFMessageListener(OFType.PACKET_IN, this);
+		
+		// Add as REST interface
+		restApiService.addRestletRoutable(new FlowReservationWebRoutable());
 
 	}
 
