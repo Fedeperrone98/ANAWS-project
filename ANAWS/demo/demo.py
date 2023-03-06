2fba4568544eba4e329aa38def3eca20ff61e587
Fedeperrone98 <f.perrone6@studenti.unipi.it>
2023-03-06 18:48:12 +0100

    start demo (demo.sh, demo.py), add command mininet

---
+++ ANAWS/demo/demo.py
@@ -0,0 +1,26 @@
+import sys
+import requests
+
+ip_controller = "8.8.8.8"
+
+def do_post_request(ip, dest, data_load):
+    my_obj = {"dest": dest, "dataload": data_load}
+    res = requests.post(ip, json=my_obj)
+    return res
+
+def main(argv):
+    dest = argv[1]
+    data_load = argv[2]
+    res = do_post_request(ip_controller, dest, data_load)
+    if not res.ok:
+        print(res.text)
+        sys.exit(-1)
+    else:
+        print(res.text)
+        sys.exit(0)
+
+if __name__ == "__main__":
+    if len(sys.argv) < 3:
+        print("You have to pass 2 parameter")
+        sys.exit()
+    main(sys.argv[1:])
---
+++ ANAWS/demo/demo.sh
@@ -0,0 +1,30 @@
+#! /bin/sh
+
+#start mininet network
+sudo mn --custom topology.py --topo fattree --mac --switch ovsk --controller remote,ip=127.0.0.1,port=6653,protocols=OpenFlow13
+
+echo "Enter src"
+read src
+echo "Enter dest"
+read dest
+echo "Enter data load (GB)"
+read dataload
+
+#open source terminal
+xterm $src
+
+#generation of a file with dimension dataload GB
+dataload += "G"
+$ truncate -s $dataload test.txt
+
+#execute python script
+res = $(python3 demo.py $dest $dataload)
+
+if [ "$res" == "0" ]; then
+    echo "New flow correctly subscribed"
+    
+    #send file via scp
+    scp -v ~/test.txt root@$dest:~/test.txt
+else
+    echo "No available flow"
+fi
